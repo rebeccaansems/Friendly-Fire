76545d0cab066428d359ea75134dfdbbf2f980d2
Rebecca Ansems <ansems_rebecca@hotmail.com>
2019-02-07 02:27:43 -0400

    Line does not intersect with projectiles anymore

--- Assets/MainAssets/Code/LineController.cs
+++ Assets/MainAssets/Code/LineController.cs
@@ -7,10 +7,12 @@ public class LineController : MonoBehaviour
     public RaycastHit2D[] raycastHits;
 
     private LineRenderer line;
+    private LayerMask ignoredLayers;
 
     void Start()
     {
         line = gameObject.GetComponent<LineRenderer>();
+        ignoredLayers = 1 << LayerMask.NameToLayer("Projectile Player") & 1 << LayerMask.NameToLayer("Projectile Enemy");
     }
 
     private void Update()
@@ -22,7 +24,7 @@ public class LineController : MonoBehaviour
     {
         line.SetPosition(0, this.transform.position);
 
-        raycastHits = Physics2D.RaycastAll(transform.position, transform.up, 10);
+        raycastHits = Physics2D.RaycastAll(transform.position, transform.up, 10, ignoredLayers);
         
         if (raycastHits.Length > 1)
         {
