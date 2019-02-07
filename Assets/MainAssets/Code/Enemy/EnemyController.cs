3c34e19aef4352a794bed07139163627d92a4e1c
Rebecca Ansems <ansems_rebecca@hotmail.com>
2019-02-07 19:35:33 -0400

    Spaceship explodes when hit with projectile

--- Assets/MainAssets/Code/Enemy/EnemyController.cs
+++ Assets/MainAssets/Code/Enemy/EnemyController.cs
@@ -7,6 +7,13 @@ public class EnemyController : MonoBehaviour
     public void Die()
     {
         GameController.instance.RemoveFromEnemyRoster(this.gameObject);
+        this.GetComponent<Animator>().SetBool("isDead", true);
+        StartCoroutine(DestroyOnAnimationFinish());
+    }
+
+    private IEnumerator DestroyOnAnimationFinish()
+    {
+        yield return new WaitForSeconds(this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length);
         Destroy(this.gameObject);
     }
 }
--- Assets/MainAssets/Code/Player/PlayerController.cs
+++ Assets/MainAssets/Code/Player/PlayerController.cs
@@ -6,6 +6,13 @@ public class PlayerController : MonoBehaviour
 {
     public void Die()
     {
+        this.GetComponent<Animator>().SetBool("isDead", true);
+        StartCoroutine(DestroyOnAnimationFinish());
+    }
+
+    private IEnumerator DestroyOnAnimationFinish()
+    {
+        yield return new WaitForSeconds(this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length);
         Destroy(this.gameObject);
     }
 }
