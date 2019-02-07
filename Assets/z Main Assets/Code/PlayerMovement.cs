6c76880818f25b927077a250f42dbac04e46ddf0
Rebecca Ansems <ansems_rebecca@hotmail.com>
2019-02-06 21:58:48 -0400

    Added one touch rotation

---
+++ Assets/z Main Assets/Code/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DigitalRubyShared;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float rotateSpeed;
+
+    private RotateGestureRecognizer rotationGesture = new RotateGestureRecognizer();
+
+    private void Start()
+    {
+        FingersScript.Instance.AddGesture(rotationGesture);
+        rotationGesture.StateUpdated += RotationGestureUpdated;
+    }
+
+    private void RotationGestureUpdated(GestureRecognizer gesture)
+    {
+        if (gesture.State == GestureRecognizerState.Began && !GestureIntersectsSprite(gesture, this.gameObject))
+        {
+            gesture.Reset();
+        }
+        else if (gesture.State == GestureRecognizerState.Executing)
+        {
+            this.transform.Rotate(0.0f, 0.0f, rotationGesture.RotationRadiansDelta * Mathf.Rad2Deg * rotateSpeed);
+        }
+    }
+
+    private bool GestureIntersectsSprite(GestureRecognizer g, GameObject obj)
+    {
+        Renderer r = obj.GetComponent<Renderer>();
+        if (r == null)
+        {
+            return false;
+        }
+        Bounds b = r.bounds;
+        Vector3 gesturePos = Camera.main.ScreenToWorldPoint(new Vector3(g.FocusX, g.FocusY, 0.0f));
+        gesturePos.z = b.center.z;
+
+        return b.Contains(gesturePos);
+    }
+}
