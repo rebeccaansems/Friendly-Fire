8be761f87cafed83a85432b736651135a34a6a05
Rebecca Ansems <ansems_rebecca@hotmail.com>
2019-02-07 00:59:13 -0400

    Rotation can be done anywhere

--- Assets/MainAssets/Code/PlayerMovement.cs
+++ Assets/MainAssets/Code/PlayerMovement.cs
@@ -7,40 +7,28 @@ public class PlayerMovement : MonoBehaviour
 {
     public float rotateSpeed;
 
-    private RotateGestureRecognizer rotationGesture = new RotateGestureRecognizer();
+    private RotateGestureRecognizer rotateGesture;
 
     private void Start()
     {
-        FingersScript.Instance.AddGesture(rotationGesture);
-        rotationGesture.StateUpdated += RotationGestureUpdated;
+        CreateRotateGesture();
     }
 
-    private void RotationGestureUpdated(GestureRecognizer gesture)
+    private void CreateRotateGesture()
     {
-        if (gesture.State == GestureRecognizerState.Began && !GestureIntersectsSprite(gesture, this.gameObject))
+        rotateGesture = new RotateGestureRecognizer();
+        rotateGesture.StateUpdated += RotateGestureCallback;
+        FingersScript.Instance.AddGesture(rotateGesture);
+    }
+
+    private void RotateGestureCallback(GestureRecognizer gesture)
+    {
+        if (gesture.State == GestureRecognizerState.Executing)
         {
-            gesture.Reset();
-        }
-        else if (gesture.State == GestureRecognizerState.Executing)
-        {
-            float rotationAmount = rotationGesture.RotationRadiansDelta * Mathf.Rad2Deg * rotateSpeed;
+            float rotationAmount = rotateGesture.RotationRadiansDelta * Mathf.Rad2Deg * rotateSpeed;
 
             GameController.instance.EnemiesRotate(rotationAmount);
             this.transform.Rotate(0.0f, 0.0f, rotationAmount);
         }
-    }
-
-    private bool GestureIntersectsSprite(GestureRecognizer g, GameObject obj)
-    {
-        Renderer r = obj.GetComponent<Renderer>();
-        if (r == null)
-        {
-            return false;
-        }
-        Bounds b = r.bounds;
-        Vector3 gesturePos = Camera.main.ScreenToWorldPoint(new Vector3(g.FocusX, g.FocusY, 0.0f));
-        gesturePos.z = b.center.z;
-
-        return b.Contains(gesturePos);
     }
 }
