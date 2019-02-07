540778589768f0efe9f6b4e44332644771ae5266
Rebecca Ansems <ansems_rebecca@hotmail.com>
2019-02-06 22:08:48 -0400

    Added line coming from front of player

---
+++ Assets/z Main Assets/Code/PlayerRay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRay : MonoBehaviour
+{
+    LineRenderer line;
+
+    void Start()
+    {
+        line = gameObject.GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        DrawLine();
+    }
+
+    void DrawLine()
+    {
+        line.SetPosition(0, this.transform.position);
+        line.SetPosition(1, transform.up * 10 + transform.position);
+    }
+}
