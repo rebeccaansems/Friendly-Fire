3c71e847747f875da99e829da36222ee2da497aa
Rebecca Ansems <ansems_rebecca@hotmail.com>
2019-02-08 20:33:34 -0400

    Rotation is changed based on whether above/below or left/right of object

--- Assets/MainAssets/Code/Gesture Controls/GestureControl.cs
+++ Assets/MainAssets/Code/Gesture Controls/GestureControl.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GestureControl : MonoBehaviour
 {
+    public int invertRotationX = 1, invertRotationY = 1;
+
     private void Update()
     {
         if (Time.timeScale != 0)
@@ -14,21 +16,52 @@ public class GestureControl : MonoBehaviour
 #if UNITY_EDITOR
             if (Input.GetMouseButton(0))
             {
-                rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
-                rotY = Input.GetAxis("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
+                if (GameController.instance.player.transform.position.y > Camera.main.ScreenToWorldPoint(Input.mousePosition).y)
+                {
+                    rotX = -Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+                }
+                else
+                {
+                    rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+                }
+
+                if (GameController.instance.player.transform.position.x > Camera.main.ScreenToWorldPoint(Input.mousePosition).x)
+                {
+                    rotY = -Input.GetAxis("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
+                }
+                else
+                {
+                    rotY = Input.GetAxis("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
+                }
             }
 #else
             if (Input.touchCount > 0)
             {
-                rotX = Input.touches[0].deltaPosition.x;
-                rotY = Input.touches[0].deltaPosition.y;
+                if (GameController.instance.player.transform.position.y > Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position).y)
+                {
+                    rotX = -Input.touches[0].deltaPosition.x * rotSpeed * Mathf.Deg2Rad;
+                }
+                else
+                {
+                    rotX = Input.touches[0].deltaPosition.x * rotSpeed * Mathf.Deg2Rad;
+                }
+
+                if (GameController.instance.player.transform.position.x > Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position).x)
+                {
+                    rotY = -Input.touches[0].deltaPosition.y * rotSpeed * Mathf.Deg2Rad;
+                }
+                else
+                {
+                    rotY = Input.touches[0].deltaPosition.y * rotSpeed * Mathf.Deg2Rad;
+                }
             }
 #endif
             Vector3 playerPos = GameController.instance.player.transform.position;
 
-            GameController.instance.player.transform.Rotate(Vector3.forward, rotY - rotX);
+            float rotation = (invertRotationY * rotY) - (invertRotationX * rotX);
+            GameController.instance.player.transform.Rotate(Vector3.forward, rotation);
 
-            GameController.instance.EnemiesRotate(rotY - rotX);
+            GameController.instance.EnemiesRotate(rotation);
         }
     }
 }
