54fd8389f588bacbecddf5e71ec4c955061268ed
Rebecca Ansems <ansems_rebecca@hotmail.com>
2019-02-10 19:36:36 -0400

    Cleaned some code

--- Assets/MainAssets/Code/Enemy/EnemyController.cs
+++ Assets/MainAssets/Code/Enemy/EnemyController.cs
@@ -8,12 +8,6 @@ public class EnemyController : MonoBehaviour
     {
         GameController.instance.RemoveFromEnemyRoster(this.gameObject);
         this.GetComponent<Animator>().SetBool("isDead", true);
-        StartCoroutine(DestroyOnAnimationFinish());
-    }
-
-    private IEnumerator DestroyOnAnimationFinish()
-    {
-        yield return new WaitForSeconds(this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length);
-        Destroy(this.gameObject);
+        Destroy(this.gameObject, this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length);
     }
 }
