b7f9ac887aa2ab3ad17d46fef813afceff396b1b
Rebecca Ansems <ansems_rebecca@hotmail.com>
2019-02-10 16:32:59 -0400

    Added gyroscoping to camera controls

---
+++ Assets/MainAssets/Code/CameraGyro.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraGyro : MonoBehaviour
+{
+    void Update()
+    {
+        this.transform.Rotate(Mathf.Clamp(-Input.gyro.rotationRateUnbiased.x, -5, 5), Mathf.Clamp(-Input.gyro.rotationRateUnbiased.y, -5, 5), Mathf.Clamp(-Input.gyro.rotationRateUnbiased.z, -5, 5));
+    }
+}
