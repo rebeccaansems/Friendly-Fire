f3a9c3b44c1f95de403ef218d6329c412f712ad1
Rebecca Ansems <ansems_rebecca@hotmail.com>
2019-02-11 18:43:47 -0400

    Smoke rings start at random times

--- Assets/MainAssets/Code/Enemy/EnemyController.cs
+++ Assets/MainAssets/Code/Enemy/EnemyController.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    [SerializeField]
+    private ParticleSystem particles;
+
+    public void Start()
+    {
+        var emit = particles.emission;
+        emit.SetBurst(0, new ParticleSystem.Burst(Random.Range(0.1f, 1.0f), 500));
+    }
+
+
     public void Die()
     {
+        particles.transform.parent = null;
+        particles.Stop();
+        Destroy(particles, 1);
+
         GameController.instance.RemoveFromEnemyRoster(this.gameObject);
         this.GetComponent<Animator>().SetBool("isDead", true);
         Destroy(this.gameObject, this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length);
--- Assets/MainAssets/Code/Player/PlayerController.cs
+++ Assets/MainAssets/Code/Player/PlayerController.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    private ParticleSystem particles;
+
+    public void Start()
+    {
+        var emit = particles.emission;
+        emit.SetBurst(0, new ParticleSystem.Burst(Random.Range(0.1f, 1.0f), 500));
+    }
+
     public void Die()
     {
         this.GetComponent<Animator>().SetBool("isDead", true);
         GameController.instance.gameIsPlaying = false;
-
+        
+        particles.transform.parent = null;
+        particles.Stop();
+        Destroy(particles, 1);
+        
         StartCoroutine(GameOverScreen());
     }
 
