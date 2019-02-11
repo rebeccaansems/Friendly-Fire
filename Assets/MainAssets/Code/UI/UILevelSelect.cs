fc0cf06b57ea353114639ffd89164b1302cd74c6
Rebecca Ansems <ansems_rebecca@hotmail.com>
2019-02-11 02:47:45 -0400

    Added multiple screens to level select

--- Assets/MainAssets/Code/UI/UILevelSelect.cs
+++ Assets/MainAssets/Code/UI/UILevelSelect.cs
@@ -11,7 +11,11 @@ public class UILevelSelect : UIController
     [SerializeField]
     private LevelInfo[] firstLevelsPerWorld;
 
+    [SerializeField]
+    private CanvasGroup[] contentPanels;
+
     private int currentWorld;
+
 
     public static UILevelSelect instance;
 
@@ -22,9 +26,12 @@ public class UILevelSelect : UIController
 
     private void Start()
     {
-        worldText.text = firstLevelsPerWorld[currentWorld].worldName;
-
         Show();
+    }
+
+    private void Update()
+    {
+        worldText.text = firstLevelsPerWorld[currentWorld].worldName;
     }
 
     public void Hide()
@@ -34,6 +41,26 @@ public class UILevelSelect : UIController
 
     public void Show()
     {
+        OpenWithoutStack(contentPanels[currentWorld]);
         Open(this.GetComponent<CanvasGroup>());
+    }
+
+    public void Move(int direction)
+    {
+        currentWorld += direction;
+        if (currentWorld > contentPanels.Length - 1)
+        {
+            currentWorld = 0;
+        }
+        else if (currentWorld < 0)
+        {
+            currentWorld = contentPanels.Length - 1;
+        }
+
+        foreach (CanvasGroup panel in contentPanels)
+        {
+            CloseWithoutStack(panel);
+        }
+        OpenWithoutStack(contentPanels[currentWorld]);
     }
 }
