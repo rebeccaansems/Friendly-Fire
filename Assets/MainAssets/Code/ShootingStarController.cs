c9e6e48d3857368fc7123d871dbe5a7fe170a52e
Rebecca Ansems <ansems_rebecca@hotmail.com>
2019-02-24 16:46:30 -0400

    Added shooting star to background

---
+++ Assets/MainAssets/Code/ShootingStarController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingStarController : MonoBehaviour
+{
+    public Vector2 delayLengthSec;
+
+    private int starDelay, count, prevAnim;
+
+    void Start()
+    {
+        starDelay = (int)Random.Range(delayLengthSec.x, delayLengthSec.y) * 60;
+    }
+
+    void Update()
+    {
+        count++;
+        if (count > starDelay)
+        {
+            starDelay = (int)Random.Range(delayLengthSec.x, delayLengthSec.y) * 60;
+            count = 0;
+
+            int currAnim = Random.Range(0, 6);
+            while (currAnim == prevAnim)
+            {
+                currAnim = Random.Range(0, 6);
+            }
+            prevAnim = currAnim;
+
+            this.GetComponent<Animator>().SetInteger("starAnim", currAnim);
+        }
+
+        if (count == starDelay / 2)
+        {
+            this.GetComponent<Animator>().SetInteger("starAnim", -1);
+        }
+    }
+}
