85fde17455ad4898ebc374eff35edd3342f9fdc8
Rebecca Ansems <ansems_rebecca@hotmail.com>
2019-02-25 01:10:14 -0400

    Added level 33

--- Assets/MainAssets/Code/Projectiles/ProjectileController.cs
+++ Assets/MainAssets/Code/Projectiles/ProjectileController.cs
@@ -76,7 +76,7 @@ public class ProjectileController : MonoBehaviour
 
     IEnumerator ProjectileHasBeenShot(Collider2D collider, Collider2D shield)
     {
-        yield return new WaitForSeconds(0.05f);
+        yield return new WaitForSeconds(0.075f);
         if (collider != null && this.GetComponent<Collider2D>() != null)
         {
             Physics2D.IgnoreCollision(this.GetComponent<Collider2D>(), collider, false);
