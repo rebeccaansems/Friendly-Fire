2e893534077a28b7d63892faf120dea399b485e5
Rebecca Ansems <ansems_rebecca@hotmail.com>
2019-02-26 01:39:35 -0400

    Added credits button/display

---
+++ Assets/MainAssets/Code/UI/UICreditController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UICreditController : UIController
+{
+    public static UICreditController instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+}
